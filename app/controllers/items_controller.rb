e5eab1ef827333ef369b66361dc2c66a89aa8a75
Radmusician <mcz5ny54gwt5mvb4ua1i@gmail.com>
2022-11-21 22:15:46 +0900

    21st commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  before_action :set_item, only: :show
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,12 +20,21 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def edit
-  #   if @item.user_id == current_user.id && @item.order.nil?
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+  def edit
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+  
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+  end
 
   def show
   end
@@ -33,7 +42,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :delivery_area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
   def set_item
