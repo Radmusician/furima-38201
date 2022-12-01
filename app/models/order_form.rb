cc8116fc733b4735a59c90e50b00a7eaab9b13d3
Radmusician <mcz5ny54gwt5mvb4ua1i@gmail.com>
2022-12-02 00:26:08 +0900

    デプロイ

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -13,6 +13,7 @@ class OrderForm
     validates :token
   end
 
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Payment.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
