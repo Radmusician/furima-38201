49a8f234893ff5646b2cc4c3d907eb1dc2f7feb0
Radmusician <mcz5ny54gwt5mvb4ua1i@gmail.com>
2022-12-01 15:31:32 +0900

    商品購入機能 #4

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 RSpec.describe OrderForm, type: :model do
   before do
     @order_form = FactoryBot.build(:order_form)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_form.user_id = user.id
+    @order_form.item_id = item.id
+    # user = FactoryBot.build(:user)
+    # item = FactoryBot.build(:item)
+    # user.id = 1
+    # item.id = 1
+    sleep 0.1
   end
 
   describe '配送先情報の保存' do
@@ -29,7 +38,7 @@ RSpec.describe OrderForm, type: :model do
         expect(@order_form).to be_valid
       end
       it '電話番号が11番桁以内かつハイフンなしであれば保存できる' do
-        @order_form.phone_number = 12_345_678_910
+        @order_form.phone_number = '01234567891'
         expect(@order_form).to be_valid
       end
       it '建物名は空でも保存できる' do
@@ -39,20 +48,20 @@ RSpec.describe OrderForm, type: :model do
     end
 
     context '配送先情報の保存ができないとき' do
-      # it 'user_idが空だと保存できない' do
-      #   @order_form.user_id = nil
-      #   @order_form.valid?
-      #   expect(@order_form.errors.full_messages).to include("User can't be blank")
-      # end
-      # it 'item_idが空だと保存できない' do
-      #   @order_form.item_id = nil
-      #   @order_form.valid?
-      #   expect(@order_form.errors.full_messages).to include("Item can't be blank")
-      # end
+      it 'user_idが空だと保存できない' do
+        @order_form.user_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと保存できない' do
+        @order_form.item_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Item can't be blank")
+      end
       it '郵便番号が空だと保存できない' do
         @order_form.postcode = nil
         @order_form.valid?
-        expect(@order_form.errors.full_messages).to include("Postcode can't be blank", 'Postcode is invalid. Include hyphen(-)')
+        expect(@order_form.errors.full_messages).to include("Postcode can't be blank")
       end
       it '郵便番号にハイフンがないと保存できない' do
         @order_form.postcode = 1_234_567
@@ -90,7 +99,7 @@ RSpec.describe OrderForm, type: :model do
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
       it '電話番号が12桁以上あると保存できない' do
-        @order_form.phone_number = 12_345_678_910_123_111
+        @order_form.phone_number = '012345678912'
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
@@ -100,7 +109,7 @@ RSpec.describe OrderForm, type: :model do
         expect(@order_form.errors.full_messages).to include("Token can't be blank")
       end
       it '電話番号は9桁以下では保存できない' do
-        @order_form.phone_number = 12_345_678_9
+        @order_form.phone_number = '012345678'
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
